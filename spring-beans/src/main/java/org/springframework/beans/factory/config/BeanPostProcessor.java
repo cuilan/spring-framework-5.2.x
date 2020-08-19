d1fb7188014e7b788cb752188f5a07be07b2eeed
zhangyan <zhangyan@gozap.com>
2020-08-19 16:36:16 +0800

    and git ignore file spring-aop/src/target

--- spring-beans/src/main/java/org/springframework/beans/factory/config/BeanPostProcessor.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/config/BeanPostProcessor.java
@@ -20,6 +20,24 @@ import org.springframework.beans.BeansException;
 import org.springframework.lang.Nullable;
 
 /**
+ * BeanPostProcessor是Spring框架的一个扩展点（不止一个）
+ * 通过实现BeanPostProcessor接口，程序员可以干预bean的实例化的过程，从而减轻了BeanFactory的负担，
+ * 这个接口可以设置多个，会形成一个列表，然后依次执行。
+ * 比如，AOP就是在bean实例化后期将切面逻辑织入bean实例中的，AOP也正是通过BeanPostProcessor和IOC容器建立联系的
+ * （由Spring提供的默认的PostProcessor，Spring提供了很多默认的PostProcessor的实现类）。
+ * 实现类：
+ * 1.ApplicationContextAwareProcessor {@link org.springframework.context.support.ApplicationContextAwareProcessor}
+ * 2.InitDestroyAnnotationBeanPostProcessor {@link org.springframework.beans.factory.annotation.InitDestroyAnnotationBeanPostProcessor}
+ * 3.InstantiationAwareBeanPostProcessor {@link org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor}
+ * 4.CommonAnnotationBeanPostProcessor {@link org.springframework.context.annotation.CommonAnnotationBeanPostProcessor}
+ * 5.AutowiredAnnotationBeanPostProcessor {@link org.springframework.beans.factory.annotation.AutowiredAnnotationBeanPostProcessor}
+ * 6.RequiredAnnotationBeanPostProcessor {@link org.springframework.beans.factory.annotation.RequiredAnnotationBeanPostProcessor}
+ * 7.BeanValidationPostProcessor {@link org.springframework.validation.beanvalidation.BeanValidationPostProcessor}
+ * 8.AbstractAutoProxyCreator {@link org.springframework.aop.framework.autoproxy.AbstractAutoProxyCreator}
+ * 9.BeanFactoryPostProcessor {@link org.springframework.beans.factory.config.BeanFactoryPostProcessor}
+ * 10.ConfigurableListableBeanFactory {@link org.springframework.beans.factory.config.ConfigurableListableBeanFactory}
+ * ...
+ * <p>
  * Factory hook that allows for custom modification of new bean instances &mdash;
  * for example, checking for marker interfaces or wrapping beans with proxies.
  *
@@ -49,11 +67,11 @@ import org.springframework.lang.Nullable;
  *
  * @author Juergen Hoeller
  * @author Sam Brannen
- * @since 10.10.2003
  * @see InstantiationAwareBeanPostProcessor
  * @see DestructionAwareBeanPostProcessor
  * @see ConfigurableBeanFactory#addBeanPostProcessor
  * @see BeanFactoryPostProcessor
+ * @since 10.10.2003
  */
 public interface BeanPostProcessor {
 
@@ -63,7 +81,8 @@ public interface BeanPostProcessor {
 	 * or a custom init-method). The bean will already be populated with property values.
 	 * The returned bean instance may be a wrapper around the original.
 	 * <p>The default implementation returns the given {@code bean} as-is.
-	 * @param bean the new bean instance
+	 *
+	 * @param bean     the new bean instance
 	 * @param beanName the name of the bean
 	 * @return the bean instance to use, either the original or a wrapped one;
 	 * if {@code null}, no subsequent BeanPostProcessors will be invoked
@@ -88,7 +107,8 @@ public interface BeanPostProcessor {
 	 * {@link InstantiationAwareBeanPostProcessor#postProcessBeforeInstantiation} method,
 	 * in contrast to all other {@code BeanPostProcessor} callbacks.
 	 * <p>The default implementation returns the given {@code bean} as-is.
-	 * @param bean the new bean instance
+	 *
+	 * @param bean     the new bean instance
 	 * @param beanName the name of the bean
 	 * @return the bean instance to use, either the original or a wrapped one;
 	 * if {@code null}, no subsequent BeanPostProcessors will be invoked
